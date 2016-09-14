37fa15db9acfbdafd8bf2f79e47e74d5f8453e02
sankit acharya <sankit.acharya@gmail.com>
2016-09-15 01:43:34 +0530

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,13 @@
+
+
+var Hello = React.createClass({
+    render: function() {
+      return (
+        <div className="Hello">
+          Yeah lets start the Project :)
+        </div>
+      );
+    }
+});
+
+ReactDOM.render(<Hello />,getElementById('content'));
