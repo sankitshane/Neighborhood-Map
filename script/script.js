2db43e32bf2739f9b5faf117688848713de1ad4c
sankit acharya <sankit.acharya@gmail.com>
2016-09-19 15:27:04 +0530

    Geolocation added

--- script/script.js
+++ script/script.js
@@ -5,7 +5,32 @@ function initMap() {
     center: {lat: 40.7413549, lng: -73.9980244},
     zoom : 13
   });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+  if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
 
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+      }
+      else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
 }
 
 $('#nav-icon').click(function(){
