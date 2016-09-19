1859362520728409998e1c2789003a2e5544ed88
sankit acharya <sankit.acharya@gmail.com>
2016-09-19 15:39:23 +0530

    Added knockout clicks

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+
 var map;
 
 function initMap() {
@@ -6,32 +7,36 @@ function initMap() {
     zoom : 13
   });
   var infoWindow = new google.maps.InfoWindow({map: map});
-  if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
-
-          infoWindow.setPosition(pos);
-          infoWindow.setContent('Location found.');
-          map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-      }
-      else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+var viewModel = {
+  geolocate: function() {
+    if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+            }, function() {
+              handleLocationError(true, infoWindow, map.getCenter());
+            });
+        }
+        else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+          }
+  },
+  handleLocationError: function(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
       'Error: The Geolocation service failed.' :
       'Error: Your browser doesn\'t support geolocation.');
-}
+  }
+};
 
 $('#nav-icon').click(function(){
   $(this).toggleClass('open');
