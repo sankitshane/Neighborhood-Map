72b5d91d12fa9e11467dc5fb653ac1ad020e4df8
sankit acharya <sankit.acharya@gmail.com>
2016-09-21 00:38:21 +0530

    Added autocomplete

--- script/script.js
+++ script/script.js
@@ -5,7 +5,55 @@ function initMap() {
     center: {lat: 40.7413549, lng: -73.9980244},
     zoom : 13
   });
+
   var infoWindow = new google.maps.InfoWindow({map: map});
+
+  var autocomplete = new google.maps.Autocomplete($('#search_place'));
+  autocomplete.bindTo('bounds', map);
+
+  var marker = new google.maps.Marker({
+      map: map,
+      anchorPoint: new google.maps.Point(0, -29)
+  });
+  autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+          }
+
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setIcon(/** @type {google.maps.Icon} */({
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+          }));
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+
+          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+          infowindow.open(map, marker);
+        });
+        
   var status ;
   $('#id-name--1').change(function() {
   status = $('#id-name--1').prop("checked");
